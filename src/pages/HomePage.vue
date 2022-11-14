d990f4d253f1b4c45df8bab5236f0ec8c73e3205
ignacio.palos <ignacio.palos@tecnoaplicada.mx>
2022-11-14 17:25:00 -0600

    subir para revision

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -3,9 +3,11 @@
     <NavBar></NavBar>
     <main-carrousel></main-carrousel>
     <seccion-servicios></seccion-servicios>
-    <placeholder-component title="Comunicación Social" height="height:150px" description="Componente que muestra links a páginas de interes - MAGUILA"/>
-    <placeholder-component title="Noticias" height="height:175px" description="Componente que muestra entradas del blog - MAGUILA"/>
-    <placeholder-component title="Acciones FGJEM" height="height:150px" description="Links a más páginas de interés - MAGUILA"/>
+    <comunicacion-social></comunicacion-social>
+    <div class="px-4">
+      <grid-noticias></grid-noticias>
+      <GridAccionesFGJEM></GridAccionesFGJEM>
+    </div>
     <seccion-contacto></seccion-contacto>
     <grid-footer></grid-footer>
 
@@ -14,13 +16,15 @@
 
 <script>
 import NavBar from '@/organisms/NavBar.vue';
-import PlaceholderComponent from '../organisms/PlaceholderComponent.vue'
 import SeccionContacto from '../organisms/SeccionContacto.vue';
 import GridFooter from '../organisms/GridFooter.vue';
 import MainCarrousel from '../organisms/MainCarrousel.vue';
 import SeccionServicios from '../organisms/SeccionServicios.vue';
+import ComunicacionSocial from '../organisms/ComunicacionSocial.vue';
+import GridNoticias from '../organisms/GridNoticias.vue';
+import GridAccionesFGJEM from '../organisms/GridAccionesFGJEM.vue';
 export default {
-  components: { PlaceholderComponent, NavBar, SeccionContacto, GridFooter, MainCarrousel, SeccionServicios },
+  components: { NavBar, SeccionContacto, GridFooter, MainCarrousel, SeccionServicios, ComunicacionSocial, GridNoticias, GridAccionesFGJEM, },
 
 }
 </script>
