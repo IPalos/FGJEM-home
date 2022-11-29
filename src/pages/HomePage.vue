f5b92f4f59d79acf27031bf771d76bb9797e5b33
vikoss <vikoss.yiss@gmail.com>
2022-11-29 09:16:44 -0600

    add button with scroll actions

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,6 +2,11 @@
   <div>
     <NavBar></NavBar>
     <main-carrousel></main-carrousel>
+    <div class="container-seccion">
+      <div class="item-seccion" @click="goToScroll('comunicacion-social')">Comunicación social</div>
+      <div class="item-seccion" @click="goToScroll('noticias')">Noticias</div>
+      <div class="item-seccion" @click="goToScroll('acciones-fgjem')">Acciones FGJEM</div>
+    </div>
     <social-banner/>
     <seccion-servicios></seccion-servicios>
     <comunicacion-social></comunicacion-social>
@@ -25,13 +30,35 @@ import ComunicacionSocial from '../organisms/ComunicacionSocial.vue';
 import GridNoticias from '../organisms/GridNoticias.vue';
 import GridAccionesFGJEM from '../organisms/GridAccionesFGJEM.vue';
 import SocialBanner from '../molecules/SocialBanner.vue';
-// import ModalFGJEM from '../organisms/ModalFGJEM.vue';
+
 export default {
-  components: { NavBar, SeccionContacto, GridFooter, MainCarrousel, SeccionServicios, ComunicacionSocial, GridNoticias, GridAccionesFGJEM, SocialBanner,  },
+  components: { NavBar, SeccionContacto, GridFooter, MainCarrousel, SeccionServicios, ComunicacionSocial, GridNoticias, GridAccionesFGJEM, SocialBanner },
+  methods: {
+    goToScroll(id) {
+      const container = this.$el.querySelector(`#${id}`);
+      container.scrollIntoView()
+    },
+  }
 
 }
 </script>
 
-<style>
-
+<style scoped>
+.container-seccion {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+}
+.item-seccion {
+  padding: 0px 50px;
+  height: 50px;
+  border: 1px;
+  border-color: #ccc;
+  border-style: solid;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
 </style>
